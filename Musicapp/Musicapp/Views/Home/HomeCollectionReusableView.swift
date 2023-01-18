f40eddd303d805aa9e3828db1825f6d4204423ca
MacBook <Macbook@MacBook-Pro-de-MACBOOK.local>
2023-01-18 16:36:24 -0600

    Actualizacion de assets y vista Home

---
+++ Musicapp/Musicapp/Views/Home/HomeCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionReusableView.swift
+//  Musicapp
+//
+//  Created by MacBook on 18/01/23.
+//
+
+import UIKit
+
+class HomeCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
