04bd8e8038f41b51ed64fda7fe108c688fb0eb78
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2023-01-18 18:14:56 -0600

    Eduardo Batista - Router

--- Musicapp/Musicapp/Presenters/SongPlayerPresenter.swift
+++ Musicapp/Musicapp/Presenters/SongPlayerPresenter.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+class SongPlayerPresentar {
+    // Router
+    
+    weak var router: SongsRouter?
+    
+    // Interactor
+    
+    // weak var interactor:
+    
+    // View
+    
+    // Subscribers
+    
+    
+    // Open (Connect / Load / Initialize)
+    
+    // Close (Disconnect / Unload / Deinitialize)
+    
+    // Operations (From Views)
+    
+}
