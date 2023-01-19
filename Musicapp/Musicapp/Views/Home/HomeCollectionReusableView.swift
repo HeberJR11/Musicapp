0196b79b05065b186426231a7a6f5ca6803119d4
BrianJM21 <brianjmoedano@gmail.com>
2023-01-18 18:11:36 -0600

    Interactor y HomeView BJM

--- Musicapp/Musicapp/Views/Home/HomeCollectionReusableView.swift
+++ Musicapp/Musicapp/Views/Home/HomeCollectionReusableView.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class HomeCollectionReusableView: UICollectionReusableView {
+    
+    // Enlace a los Labels del CollectionReusableView.
+    @IBOutlet weak var generoLabel: UILabel!
+    
+    @IBOutlet weak var numeroCancionesLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+    }
+    
+    // Función que configura las etiquetas de la colecciópn, en Género musical y número de canciones disponibles.
+    func setupGeneroCancionesLabel(genero: String, canciones: String) {
+        
+        self.generoLabel.text = genero
+        self.numeroCancionesLabel.text = canciones
     }
     
 }
